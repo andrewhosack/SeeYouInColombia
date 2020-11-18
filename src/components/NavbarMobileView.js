c6aed64e15c368db1b040a6303ab5f3e38a2e6b3
andrewhosack <49813742+andrewhosack@users.noreply.github.com>
2020-11-18 12:17:59 -0500

    Add files via upload

--- src/components/NavbarMobileView.js
+++ src/components/NavbarMobileView.js
@@ -12,7 +12,7 @@ const NavbarDropDown = class extends React.Component {
   render() {
     return (
       <div className="navbar-start has-text-centered" style={{display:`flex`,justifyContent:`right`, left:`0`, right:`0`, marginLeft:`auto`, marginRight:`auto`, position:`absolute`}}>
-        <div className="mobileView box" style={{boxShadow:`5px 5px 25px 0 rgba(46,61,73,.2)`,maxWidth:`450px`,display:`flex`, flexDirection:`column`,borderTop: `1.5px solid red`, padding: `10px` ,borderTopRightRadius: `12px`,borderTopLeftRadius: `12px`,margin:`20px`,marginTop:`0px`}}>
+        <div className="mobileView box" style={{boxShadow:`5px 5px 25px 0 rgba(46,61,73,.2)`,maxWidth:`450px`,display:`flex`, flexDirection:`column`,borderTop: `1.5px solid red`, padding: `10px` ,borderTopRightRadius: `12px`,borderTopLeftRadius: `12px`,margin:`20px`,marginTop:`0px`,overflowY: `scroll`,height:`650px`}}>
                 
                 <div style={{display:`flex`,flexDirection:`row`}}>
             
@@ -28,7 +28,7 @@ const NavbarDropDown = class extends React.Component {
                           <div style={{width:`35px`}}>
                             <FontAwesomeIcon className="test" style={{width:`35px`, height:`70px`,color:`rgb(255, 68, 0)`}}  icon={faFilePowerpoint} />
                           </div>
-                          <div style={{display:`flex`,flexDirection:`column`, marginLeft:`10px`, marginRight:`30px`}}>
+                          <div style={{display:`flex`,flexDirection:`column`, marginLeft:`20px`, marginRight:`30px`}}>
                             <span style={{textAlign:`left`, fontWeight:`bold`, fontSize:`22px`}}>Presentations</span>
                             <span style={{textAlign:`left`,color:`black`,fontSize:`18px`}}>Join us for a presentation about Colombia!</span>
                           </div>
@@ -39,7 +39,7 @@ const NavbarDropDown = class extends React.Component {
                           <div style={{width:`35px`}}>
                             <FontAwesomeIcon style={{width:`35px`, height:`70px`,color:`rgb(255, 68, 0)`}}  icon={faSchool} />
                           </div>
-                          <div style={{display:`flex`,flexDirection:`column`, marginLeft:`10px`, marginRight:`30px`}}>
+                          <div style={{display:`flex`,flexDirection:`column`, marginLeft:`20px`, marginRight:`30px`}}>
                             <span style={{textAlign:`left`, fontWeight:`bold`, fontSize:`22px`}}>Learn Spanish</span>
                             <span style={{textAlign:`left`,color:`black`,fontSize:`18px`}}>Hablas Espanol? Learn Colombian Spanish with us!</span>
                           </div>
@@ -50,7 +50,7 @@ const NavbarDropDown = class extends React.Component {
                           <div style={{width:`35px`}}>
                             <FontAwesomeIcon style={{width:`35px`, height:`70px`,color:`rgb(255, 68, 0)`}}  icon={faPassport} />
                           </div>
-                          <div style={{display:`flex`,flexDirection:`column`, marginLeft:`10px`, marginRight:`30px`}}>
+                          <div style={{display:`flex`,flexDirection:`column`, marginLeft:`20px`, marginRight:`30px`}}>
                             <span style={{textAlign:`left`, fontWeight:`bold`, fontSize:`22px`}}>Travel Consulting</span>
                             <span style={{textAlign:`left`,color:`black`,fontSize:`18px`}}>Need some help managing and planning your trip? We can help!</span>
                           </div>
@@ -60,7 +60,7 @@ const NavbarDropDown = class extends React.Component {
                         <div style={{width:`35px`}}>
                           <FontAwesomeIcon style={{width:`35px`, height:`70px`,color:`rgb(255, 68, 0)`}}  icon={faUserLock} />
                         </div>
-                        <div style={{display:`flex`,flexDirection:`column`, marginLeft:`10px`, marginRight:`30px`}}>
+                        <div style={{display:`flex`,flexDirection:`column`, marginLeft:`20px`, marginRight:`30px`}}>
                           <span style={{textAlign:`left`, fontWeight:`bold`, fontSize:`22px`}}>Members Area</span>
                           <span style={{textAlign:`left`,color:`black`,fontSize:`18px`}}>Join our members area for exclusive Colombian content!</span>
                         </div>
@@ -70,19 +70,23 @@ const NavbarDropDown = class extends React.Component {
                    <Link className="navbar-item" to="/purchase/purchase">
                     Pricing
                     </Link>
-                    {/* <Link className="navbar-item" to="/about">
-                      Who we are
-                    </Link> */}
+
                     <span  className="navbar-item" >Company</span>
+
                     <Link to="/about" className="navbar-item" style={{height:`50px`,display:`flex`, alignItems:`center`,marginLeft:`20px`,fontWeight:`lighter`}}>
                         Who we are
                       </Link>
-                      <Link to="/contact/" className="navbar-item" style={{height:`60px`,display:`flex`, alignItems:`center`,marginLeft:`20px`,fontWeight:`lighter`}}>
+                      <a href="https://calendly.com/enjoysafetravelscolombia/20-minute-colombia-consultation" className="navbar-item" style={{height:`60px`,display:`flex`, alignItems:`center`,marginLeft:`20px`,fontWeight:`lighter`}}><span>Book a Consultation Call</span></a>
+                      
+                      
+                     
+                      <span  className="navbar-item" >Resources</span>
+                      <Link to="/blog/" className="navbar-item" style={{height:`60px`,display:`flex`, alignItems:`center`,marginLeft:`20px`,fontWeight:`lighter`}}>
+                        Blog
+                      </Link>
+                    <Link to="/contact/" className="navbar-item" style={{height:`60px`,display:`flex`, alignItems:`center`,marginLeft:`20px`,fontWeight:`lighter`}}>
                         Contact Us
                       </Link>
-                      <Link className="navbar-item" to="/blog">
-                        Blog
-                    </Link>
                     </div>
                  </div>
                  </div>
